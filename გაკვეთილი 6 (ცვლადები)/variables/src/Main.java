0da6477046b2a21b1461e8ea57273126279d416e
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-10-11 21:14:47 +0400

    add lesson 6 files

---
+++ გაკვეთილი 6 (ცვლადები)/variables/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean isStudent = true;
+        boolean _asesaf = false;
+        boolean $aseqqjoiwjeas2341df = false;
+
+        System.out.println(isStudent);
+
+        byte age = 100;
+        short grams = 4000;
+        int id = 987654321;
+        long largeNumber = 10_000_000_000L;
+        System.out.println(age);
+        System.out.println(grams);
+        System.out.println(id);
+        System.out.println(largeNumber);
+
+
+        float pi = 3.14F;
+        float e = 2.71828112342134213F;
+        System.out.println(pi);
+        System.out.println(e);
+
+        double temperature = 24.5F;
+        double temperatureWithoutF = 24.5;
+        System.out.println(temperature);
+        System.out.println(temperatureWithoutF);
+        double eDouble = 2.718_281_123_421_342_13;
+        System.out.println(eDouble);
+
+
+        char euler = 'e';
+        System.out.println(euler);
+        char c = 120;
+        System.out.println(c);
+
+        String fullName = "Davit Maisuradze";
+        System.out.println("My name is " + fullName);
+
+
+
+
+
+        int number = 150;
+        long anotherNumber = number;
+        byte shortNumber = (byte) anotherNumber;
+        System.out.println(anotherNumber);
+        System.out.println(shortNumber);
+
+
+        double num = 4.67;
+        int intNum = (int) num;
+        long roundedNumber = Math.round(num);
+        System.out.println(intNum);
+        System.out.println(roundedNumber);
+    }
+}
