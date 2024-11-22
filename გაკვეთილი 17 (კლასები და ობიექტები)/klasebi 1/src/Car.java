e4e6575547a03d4a2bd06dbccac18f6702daca11
davidmaisuradze <maisuradze.davit@kiu.edu.ge>
2024-11-22 21:25:46 +0400

    add lesson 17 materials

---
+++ გაკვეთილი 17 (კლასები და ობიექტები)/klasebi 1/src/Car.java
@@ -0,0 +1,39 @@
+public class Car{
+    // v1
+//    String make = "BMW";
+//    String model = "M4";
+//    int year = 2024;
+//    String color = "Blue";
+//    double price = 55000.67;
+
+    // v2
+    String make;
+    String model;
+    int year;
+    String color;
+    double price;
+
+    public Car(String make, String model, int year, String color, double price) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.price = price;
+    }
+
+    void drive() {
+        System.out.println(make + " " + model + " დაიძრა");
+    }
+
+    void stop() {
+        System.out.println(make + " " + model + " გაჩერდა");
+    }
+
+    void turnLeft() {
+        System.out.println(make + " " + model + " მოუხვია მარცხნივ");
+    }
+
+    void turnRight() {
+        System.out.println(make + " " + model + " მოუხვია მარჯვნივ");
+    }
+}
---
+++ გაკვეთილი 17 (კლასები და ობიექტები)/klasebi 1/src/Main.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+
+//        Car myCar = new Car();
+//
+//        System.out.println("My car:");
+//        System.out.println(myCar.make);
+//        System.out.println(myCar.model);
+//        System.out.println(myCar.year);
+//        System.out.println(myCar.color);
+//        System.out.println(myCar.price);
+//        System.out.println();
+//
+//        myCar.price += 20000;
+//        myCar.model += " Competition";
+//        System.out.println("My car:");
+//        System.out.println(myCar.make);
+//        System.out.println(myCar.model);
+//        System.out.println(myCar.year);
+//        System.out.println(myCar.color);
+//        System.out.println(myCar.price);
+
+
+
+//        Car car1 = new Car();
+//        Car car2 = new Car();
+//
+//        System.out.println("Car 1: " + car1.make + " " + car1.model);
+//        System.out.println("Car 2: " + car2.make + " " + car2.model);
+
+//        int x;
+//        System.out.println(x);
+
+//        Car newCar = new Car();
+//        System.out.println(newCar.make);
+//        System.out.println(newCar.model);
+//        System.out.println(newCar.year);
+//        System.out.println(newCar.color);
+//        System.out.println(newCar.price);
+//        System.out.println();
+
+//        Car car;
+//        System.out.println(car);
+
+//        boolean[] arr = new boolean[4];
+//        System.out.println(Arrays.toString(arr));
+
+//        Car dreamCar = new Car();
+//        dreamCar.make = "Aston Martin";
+//        dreamCar.model = "Valkyrie";
+//        dreamCar.year = 2024;
+//        dreamCar.color = "Slipstream Green";
+//        dreamCar.price = 3_500_000.89;
+//
+//        System.out.println("My dream car: {");
+//        System.out.println("\tMake: " + dreamCar.make);
+//        System.out.println("\tModel: " + dreamCar.model);
+//        System.out.println("\tPrice: " + dreamCar.price);
+//        System.out.println("\tColor: " + dreamCar.color);
+//        System.out.println("\tYear: " + dreamCar.year);
+//        System.out.println("}");
+
+        // f(x) = x + 2;
+//        System.out.println(f(78));
+//        int x = f(15);
+//        System.out.println(x);
+
+//        for (int i = 0; i < 10; i++) {
+//            i = 10;
+//        }
+//        i = 10;
+
+//        Car coolCar = new Car();
+//        coolCar.make = "Mercedes";
+//        coolCar.model = "G63";
+//        coolCar.year = 2023;
+//        coolCar.color = "Black";
+//        coolCar.price = 150000.98;
+//
+//        dreamCar.drive();
+//        dreamCar.turnLeft();
+//        coolCar.drive();
+//        coolCar.turnRight();
+//        dreamCar.turnRight();
+//        coolCar.turnLeft();
+//        coolCar.stop();
+//        dreamCar.turnRight();
+//        dreamCar.stop();
+
+        Car car = new Car(
+                "Mercedes",
+                "C63",
+                2019,
+                "Silver",
+                89_999.99
+        );
+        System.out.println(car.make);
+        System.out.println(car.model);
+        System.out.println(car.year);
+        System.out.println(car.color);
+        System.out.println(car.price);
+    }
+
+    static int f(int x) {
+        return x + 2;
+    }
+}
